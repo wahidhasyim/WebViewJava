052885999fee2777a481b841d8218cb547bf5883
wahidhasyim <hasyimwahid694@gmail.com>
2020-04-21 04:03:41 +0700

    commit pertama

---
+++ app/src/main/java/com/hidsterichdev/webviewapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hidsterichdev.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

7128f120e80fc76380693d841f5d59c439a97b59
wahidhasyim <hasyimwahid694@gmail.com>
2020-04-21 04:21:07 +0700

    commit 2

--- app/src/main/java/com/hidsterichdev/webviewapp/MainActivity.java
+++ app/src/main/java/com/hidsterichdev/webviewapp/MainActivity.java
@@ -2,13 +2,59 @@ package com.hidsterichdev.webviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        progressBar = findViewById(R.id.progres_bar);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new myWebClient());
+        webView.loadUrl("https://github.com/wahidhasyim");
     }
+
+    public class myWebClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted (WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished (WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading (WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown (int keyCode, KeyEvent event) {
+        if ((keyCode==KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
